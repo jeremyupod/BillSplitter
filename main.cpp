7bd15f46ddcee2c2e1120ae94e3ec78228082412
jeremyupod <upodjeremy@gmail.com>
2026-08-20 08:40:49 -0700

    Basic Bill Splitting (Even, No Tax/Tip)

--- main.cpp
+++ main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+
+struct Item {
+    std::string name;
+    double price;
+};
 
 int main()
 {
-    std::cout << "Bill Splitter is running!" << std::endl;
+    // hardcoded receipt
+    std::vector<Item> items = {
+        {"Burger", 12.50},
+        {"Fries", 4.00},
+        {"Shake", 2.50}
+    };
+
+    std::vector<std::string> people = {"Mykah", "Ivy"};
+
+    // total of the receipt
+    double total = 0.0;
+    for (const Item& item : items){
+        total += item.price;
+    }
+
+    // split bill evenly
+    double share = total / people.size();
+    
+    // set precision
+    std::cout << std::fixed << std::setprecision(2);
+
+    std::cout << "Total: $" << total << "\n";
+    for (const std::string& person : people){
+        std::cout << person << " owes: $" << share << "\n";
+    }
 
     return 0;
 }
